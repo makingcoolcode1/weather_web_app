c8f892ddd2a7d43cb0dae8b712173b18a02691e7
zgedge1 <zachgedge@gmail.com>
2024-03-14 20:16:27 -0400

    updating code so API entry disappears when user clicks save

--- apiPrompt.js
+++ apiPrompt.js
@@ -1,16 +1,19 @@
 
 const apiModal = document.getElementById("api-modal");
 
-window.onload = function(){
-    apiModal.style.display = "block";
+window.onclick = function(event) {
+    if (event.target == apiModal) {
+        apiModal.style.display = "none";
+        
+    }
 }
 
-function closeModal(){
+function closeModal() {
     apiModal.style.display = "none";
 }
 
-function storeApiKey() {
-    const apiKey = document.getElementById("apiKeyInput")
-    console.log("API KEY; ", apiKey)
-    apiModal.style.display = "none"
+function storeApiKey(){
+    const apiKey = document.getElementById("api-key")
+    console.log("API KEY", apiKey);
+    apiModal.style.display = "none";
 }
