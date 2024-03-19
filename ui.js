5459a2c4488d26b635e0beec36c7bfee7bbde2d7
zgedge1 <zachgedge@gmail.com>
2024-03-19 19:42:47 -0400

    adding spinner

--- ui.js
+++ ui.js
@@ -2,34 +2,44 @@
 function getWeather(){
     const apiKey = document.getElementById("api-key").value;
     const citySearch = document.getElementById("city-search").value;
+    const spinner = document.getElementById("spinner");
 
-    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=metric`)
-    .then(weatherResult => {
-        if (!weatherResult.ok) {
-            console.error()
-            alert("ERROR: Unable to connect to API!")
-        }
+    spinner.style.display = "block";
 
-        return weatherResult.json()
+    setTimeout(() => {
 
-    })
-    .then(weatherData => {
-        
-        const weatherDescription = weatherData.weather[0].description;
-        const weatherTemp = weatherData.main.temp;
-        const weatherHumdity = weatherData.main.humidity;
 
-        document.getElementById("weather-output").innerHTML = `
-        <h2>Weather in ${citySearch}</h2>
-        <p>Description: ${weatherDescription}</p>
-        <p>Temperature: ${weatherTemp}</p>
-        <p>Humidity: ${weatherHumdity}</p>
-        `;
-    })
-    .catch(error => {
-        document.getElementById("weather-output").innerHTML = `<p>ERROR: Unable to find city....Please enter another city.<p>`
-        console.error(error)
-        alert("ERROR: City not found")
-    })
+        fetch(`https://api.openweathermap.org/data/2.5/weather?q=${citySearch}&appid=${apiKey}&units=metric`)
+        .then(weatherResult => {
+            if (!weatherResult.ok) {
+                console.error()
+                alert("ERROR: Unable to connect to API!")
+            }
+    
+            return weatherResult.json()
+    
+        })
+        .then(weatherData => {
+            
+            const weatherDescription = weatherData.weather[0].description;
+            const weatherTemp = weatherData.main.temp;
+            const weatherHumdity = weatherData.main.humidity;
+    
+            document.getElementById("weather-output").innerHTML = `
+            <h2>Weather in ${citySearch}</h2>
+            <p>Description: ${weatherDescription}</p>
+            <p>Temperature: ${weatherTemp}</p>
+            <p>Humidity: ${weatherHumdity}</p>
+    
+            `;
+    
+            spinner.style.display = "none";
+        })
+        .catch(error => {
+            document.getElementById("weather-output").innerHTML = `<p>ERROR: Unable to find city....Please enter another city.<p>`
+            console.error(error)
+            alert("ERROR: City not found")
+        })
+    }, 2000);
 
 }
