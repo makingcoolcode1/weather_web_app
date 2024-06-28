a0776800aecd9f45046d4bf5e897a03d971da382
zgedge1 <zachgedge@gmail.com>
2024-06-28 19:54:52 -0400

    added api prompt to web ui

--- apiPrompt.js
+++ apiPrompt.js
@@ -2,14 +2,15 @@
 const apiModal = document.getElementById("api-modal");
 
 window.onclick = function(event){
-    if (event.target == apiModal) {
-        apiModal.style.display = "none"
-        
+    if (event.target == apiModal){
+        apiModal.style.display = "none";
     }
 }
 
 function storeApiKey(){
     const apiKey = document.getElementById("api-key")
-    console.log("API KEY", apiKey)
-    apiModal.style.display = "none"
+    console.log("API KEY:", apiKey)
+    apiModal.style.display = "none";
 }
+
+
